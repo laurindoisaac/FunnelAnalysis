5466569f25fde9294a75be62ed013b142812ee1c
Lauri <116688165+laurindoisaac@users.noreply.github.com>
2025-08-10 11:17:38 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/funnelanalysis/main.go
@@ -0,0 +1,23 @@
+// cmd/funnelanalysis/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "funnelanalysis/internal/funnelanalysis"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := funnelanalysis.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
